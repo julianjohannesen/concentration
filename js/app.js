b2bbc7256dafe6ff28d14b365c76ef842ebb1442
Julian Johannesen <julian.johannesen@gmail.com>
2018-05-24 12:19:17 -0400

    Initial commit

--- js/app.js
+++ js/app.js
@@ -1,38 +1,93 @@
-/*
- * Create a list that holds all of your cards
- */
+// Get the deck element
+const deck = document.getElementById('deck');
+// Create the cards array
+// Array.from() creates an array from the static node list returned by querySelectorAll()
+const cards = Array.from(deck.querySelectorAll('.card'));
+// The two most recently chosen cards
+const openCards = [];
 
+// Shuffle and display the cards on the page
+function setup() {
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+    // Re-order the elements in the cards array
+    shuffle(cards);
+    // Create a fragment to store our appended list items, thus avoiding reflows
+    const fragment = document.createDocumentFragment();
+    // Loop through the cards in the newly shuffled cards array and append each card to our fragment.
+    for(const card of cards) {
+        fragment.appendChild(card);
+    }
+    // innerHTML overwrites the current list contents
+    deck.innerHTML = "";
+    // appendChild inserts the fragment
+    deck.appendChild(fragment);
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
 
+    let currentIndex = array.length, temporaryValue, randomIndex;
+    // Randomly swap around the values in the array for as many time as there are values
     while (currentIndex !== 0) {
+        // Set a random index number in our range
         randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
+        // Decrement the current index counter
+        currentIndex--;
+        // Store the value at the current index
         temporaryValue = array[currentIndex];
+        // These next two lines switch the places of the value at current index
+        // and the value at the random index.
+        // Change the value at the current index to the value at the random index
         array[currentIndex] = array[randomIndex];
+        // Change the value at the random index to the value at the current index
         array[randomIndex] = temporaryValue;
     }
-
     return array;
 }
 
+/* Flip the card */
+function flip(event) {
+    if (openCards.length < 2) {
+        displayIcon(event);
+        addToOpen(event);
+    }
+}
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+/* Display the card icon */
+function displayIcon(event) {
+    event.target.classList.add('show');
+}
+
+/* Add the card to the list of "open" cards */
+function addToOpen(event) {
+    openCards.push(event.target.parentElement);
+}
+
+// If the icon of card just added to the openCards list, matches the icon of a card already on the list, then add class 'match' etc.
+// NOTE - they add the match class to the li element. Same for the 'open' and 'show' classes
+function match() {
+    const cardOneClasses = openCards[0].firstElementChild.classList;
+    const cardTwoClasses = openCards[1].firstElementChild.classList;
+    let theIcon = "";
+    // Is there more than one card in openCards?
+    if (openCards.length > 1) {
+        // This loop determines whether there's a class in the most recently opened  card's class list that begins with fa-. It will store that class in the theIcon variable. This way we don't have to depend on the position of the icon class in the list of classes.
+        for (const theClass of classesArray) {
+            //Test the regular expression to see if it matches the current class in the loop
+            //If it does, then assign that class to theIcon.  This && expression is a shorthand way of wrting an if statement.
+            /fa-.+/.test(theClass) && (theIcon = theClass);
+        }
+        // Loop through the cards in openCards and if a card contains theIcon, then add the match class
+        for (const card of openCards) {
+            if (card.classList.contains(theIcon)) {
+                card.classList.toggle("match");
+                break;
+            }
+        }
+    }
+}
+
+// Shuffle the cards and display the board
+setup();
+/* If a card is clicked, "flip" it. */
+deck.addEventListener("click", flip, false);
---
+++ js/somejs.js
@@ -0,0 +1,31 @@
+const matchedCards = [];
+let numOfCards = 16;
+const openCards = [];
+let theCounter = 0;
+
+//while (matchedCards.length < numOfCards) {
+    // do the stuff
+//}
+
+// If two cards are open and are not matched, don't allow user to open more cards
+
+/* Hide the card icon. If the two flipped cards don't match, then we'll need to flip them back over with this function */
+function hideIcon(card) {
+    card.firstElementChild.classList.remove('show');
+}
+/* Remove a card from openCards if it isn't a match. If the two flipped cards don't match, then we'll need to flip them back over. */
+function removeCard() {
+    openCards.pop();
+}
+
+
+
+function increaseCounter() {
+    const star = document.createElement("li");
+    star.innerHTML = '<i class="fa fa-star"></i>';
+    document.getElementById('stars').appendChild(star);
+    theCounter++;
+    document.getElementById('moves').textContent = theCounter;
+}
+
+//document.getElementById('score-panel').innerHTML = 'Congratulations! You finished!';
