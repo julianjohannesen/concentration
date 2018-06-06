20549809a7528f5dbba330c6c126f208dbb3d314
Julian Johannesen <julian.johannesen@gmail.com>
2018-06-06 19:34:17 -0400

    Filename changes

--- js/app.js
+++ js/app.js
@@ -1,134 +1,133 @@
 /*jshint esversion: 6 */
-
-// Get the deck element
-const deck = document.getElementById("deck");
-// Create the cards array
-// Array.from() creates an array from the static node list returned by querySelectorAll()
-const cards = Array.from(deck.querySelectorAll(".card"));
-// The two most recently chosen cards
-const openCards = [];
-// The matched cards
-const matchedCards = [];
-// The move counter
-let moveCounter = 0;
-// The clock
+/*
+* VARIABLES 
+*/
+/* DOM elements */
+const header = document.querySelector("header");
+const playBttn = document.querySelector(".play");
+const innerContainer = document.querySelector(".inner-container");
+const starsUl = document.getElementById("stars");
+const starsLis = document.getElementsByClassName("stars")[0].childNodes;
+const moves = document.getElementById("moves");
 const clock = document.getElementById("clock");
-// Clock variables
-let seconds = 0;
-let minutes = 0;
-let hundredths = 0;
-let t;
+const reset = document.getElementById("reset");
+const playAgainModal = document.getElementById("modal");
+const playAgainBttn = document.getElementById("play-again");
+const deck = document.getElementById("deck");
+const cards = Array.from(deck.querySelectorAll(".card"));
+/* Arrays */
+const openCards = [];
+const matchedCards = [];
+/* Counters */
+let moveCounter = 0, seconds = 0, minutes = 0,  hundredths = 0; i = 10;
+/* Timers */
+let flipDownTimer, clockTimer, modalTimer, starHandlerTimer;
 
-// Shuffle and display the cards on the page
+/* Setup play, initialize or re-initialize counters, timers, etc. */
 function setup(e) {
-    // Prevent page reload, if the user clicks the "play again" button
-    if (e) {
+    if(e){
+        /* If the user initiated setup by pressing a button or link, prevent the default behavior */
         e.preventDefault();
+        /* If the user clicked play on the splashscreen, hide the splash and show the game board */
+        if(e.target.id == "play"){
+            header.style.display = "none";
+            innerContainer.style.display = "block";
+        }
     }
-    // Remove the show-modal class from the modal
-    document.getElementById("play-again-modal").classList.remove("show-modal");
-
-    // Re-order the elements in the cards array
+    /* If the clicked play on the modal, hide the modal */
+    playAgainModal.classList.remove("show-modal");
+    /* Shuffle cards */
     shuffle(cards);
-    // Create a fragment to store our appended list items, thus avoiding reflows
+    /* Create a fragment to prevent reflows and repaints */
     const fragment = document.createDocumentFragment();
-    // Loop through the cards in the newly shuffled cards array and append each card to our fragment.
+    /* For each card, strip out any classes and append the card to the fragment */
     for (const card of cards) {
-        // Remove all open, show, match classes from cards
         card.classList.remove("no-match", "open", "match");
-        // Append the card to the document fragment
         fragment.appendChild(card);
     }
-    // innerHTML overwrites the current list contents
+    /* Overwrite the deck html with nothing */
     deck.innerHTML = "";
-    // appendChild inserts the fragment
+    /* Append the fragment to the deck */
     deck.appendChild(fragment);
-    // Clear the clock
+    /* Clear the open cards array */
+    openCards.splice(0,2);
+    /* Clear the matched cards array */
+    matchedCards.splice(0,16);
+    /* Clear the move counter and moves */
+    moveCounter = 0
+    moves.textContent = moveCounter;
+    /* Reset star counter and show hidden stars */
+    i = 10;
+    starsLis.forEach( theLi => theLi.firstElementChild.classList.remove("hide"));
+    /* Clear all timers */
+    stopClock();
     clearClock();
-    // Start the clock
-    startTimeout();
+    clearTimeout(modalTimer);
 }
 
-// Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-
     let currentIndex = array.length,
         temporaryValue, randomIndex;
-    // Randomly swap around the values in the array for as many time as there are values
     while (currentIndex !== 0) {
-        // Set a random index number in our range
         randomIndex = Math.floor(Math.random() * currentIndex);
-        // Decrement the current index counter
         currentIndex--;
-        // Store the value at the current index
         temporaryValue = array[currentIndex];
-        // These next two lines switch the places of the value at current index
-        // and the value at the random index.
-        // Change the value at the current index to the value at the random index
         array[currentIndex] = array[randomIndex];
-        // Change the value at the random index to the value at the current index
         array[randomIndex] = temporaryValue;
     }
     return array;
 }
 
-/* Flip the cards and see whether they match. If so add them to matched, etc. If not flip them back over. */
+/* The turn function contains much of the game logic */
 function turn(e) {
-    /* Only a click on a list item is accepted, and only if that card hasn't already been clicked on. We don't want to allow clicks between cards or clicking on the same card more than once in a turn */
+    /* If the user clicks on a card that isn't already open or a matched card, then... */
     if (e.target.nodeName.toLowerCase() == "li" && !openCards.includes(e.target) && !matchedCards.includes(e.target)) {
-        // If this is your first or second card, do the following. Clicking on a third card does nothing.
+        /* If this is a new game, restart the clock and star ratings */
+        if (moveCounter === 0) {
+            startTimeout();
+            starRating();
+        }
+        /* If there are 0 or 1 cards showing ... */
         if (openCards.length === 0 || openCards.length === 1) {
-            // Flip over the card
+            /* Flip the card up */
             flipUp(e);
-            // Add the new card to the openCards array
+            /* Add the card to the open cards array */
             addToOpenCards(e);
-            // Increase the move counter
-            increaseCounter();
-            // If this is your second card flip, do the following
+            /* Increase the move counter */
+            increaseMoveCounter();
+            /* If two cards are showing ... */
             if (openCards.length === 2) {
-                // If the cards match
+                /* If they're a match, add the match class and add them to the matched cards array */
                 if (matched()) {
-                    // ...then add the match class
                     addMatch();
-                    // ... and add these cards to the matched cards array
                     addToMatchedCards();
-                    // Or, if the cards don't match
                 } else {
-                    // ... add the no-match class to both cards.
+                    /* Otherwise, add the no-match class to the 2 cards and after a second or so, flip them back down again */
                     openCards[0].classList.add("no-match");
                     openCards[1].classList.add("no-match");
-                    // Show the cards for a couple of seconds, before flipping them back over
-                    setTimeout(flipDown, 1600);
+                    const flipDownTimer = setTimeout(flipDown, 1200);
                 }
             }
         }
     }
 }
 
-/* Display the card icon */
-function flipUp(e) {
-    // Add the "show" class to the li, not the i
-    e.target.classList.add("open");
-}
+/* Flip up a card, when it is clicked on */
+function flipUp(e){e.target.classList.add("open");}
 
-/* Add the card to the list of "open" cards */
-function addToOpenCards(e) {
-    openCards.push(e.target);
-}
+/* Add the flipped card to the open cards array */
+function addToOpenCards(e){openCards.push(e.target);}
 
-// Increase the move counter each time a card is flipped up
-function increaseCounter() {
+/* Increase the move counter */
+function increaseMoveCounter() {
     moveCounter++;
-    document.getElementById("moves").textContent = moveCounter;
+    moves.textContent = moveCounter;
 }
 
-/* If openCards contains two cards, then get the icon class of the first card and compare it to the icon class of the second card to see whether they match.
-NOTE - Add the 'match' 'open' and 'show' classesclass to the li element, not the i element. */
+/* Use a regular expression to check to see if the icon on the previusly flipped card matches the icon on this card and, if so, return true */
 function matched() {
-    // The list of classes on the i tag for both cards (should be icon and icon-whatever)
     const cardOneClasses = openCards[0].firstElementChild.classList;
     const cardTwoClasses = openCards[1].firstElementChild.classList;
-    /* Then loop to check each class in the first card's class list to determine whether there's a class that begins with 'icon-' and if that same class is also contained in the second card's class list. Using the regular expression means that we don't have to depend on the position of the icon class in the list of classes. */
     for (const theClass of cardOneClasses) {
         if (/icon-.+/.test(theClass) && cardTwoClasses.contains(theClass)) {
             return true;
@@ -136,158 +135,114 @@ function matched() {
     }
 }
 
+/* Add the matched class to matched cards */
 function addMatch() {
-    // Add the match class to the two matching cards, adding the class to the li, not to the i
     openCards[0].classList.add("match");
     openCards[1].classList.add("match");
 }
 
+/* Add the matched card to the matched cards array */
 function addToMatchedCards() {
-    // Add the cards to the matchedCards array
     matchedCards.push(openCards[0], openCards[1]);
-    // And remove them from the openCards array. There are only two, so we'll just pop them off one after the other.
+    /* After they've been added, remove them from the open cards array */
     openCards.pop();
     openCards.pop();
+    /* Check to see if the game is over */
     gameOver();
-
 }
 
+/* Flip cards back down when they don't match, removing the open and no-match classes and removing them from the open cards array */
 function flipDown() {
-    // If there's no match, flip the cards back over
-    console.log(openCards);
-    removeClasses(openCards, "no-match", "open");
-    
-    // openCards[0].classList.remove("no-match", "open");
-    // openCards[1].classList.remove("no-match", "open");
-    // And remove them from the openCards array
+    openCards[0].classList.remove("no-match", "open");
+    openCards[1].classList.remove("no-match", "open");
     openCards.pop();
     openCards.pop();
 }
 
+/* If there are 16 cards in the matched cards array, the game is over. Stop the clock and pop up the modal. */
 function gameOver() {
-    // If the user has matched all of the cards, do the following
     if (matchedCards.length === 16) {
-        // Stop the clock
         stopClock();
-        // Show the play again modal
-        document.getElementById("play-again-modal").classList.add("show-modal");
+        modalTimer = setTimeout(modalSetup, 1500);
+        return true;
     }
 }
 
-/*
- * THE CLOCK
- * Source articles used to create this code:
- *
- * https://codepen.io/cathydutton/
- * https://stackoverflow.com/questions/729921/settimeout-or-setinterval
- * https://johnresig.com/blog/how-javascript-timers-work/
- * https://javascript.info/settimeout-setinterval
- *
- */
-
+/* Update the clock */
 function updateClock() {
-    // Increment time
+    /* This section defines hundredths, seconds and minutes */
     hundredths++;
-    /* When you reach 1 second, increment seconds and set hundredths back to zero. when you reach 1 minute, increment minutes and set seconds back to zero. */
     if (hundredths > 99) {
         hundredths = 0;
         seconds++;
-        if (seconds >= 60) {
+        if (seconds > 59) {
             seconds = 0;
             minutes++;
         }
     }
-        // Now set the clock display
-        clock.textContent = (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds) + ":" + (hundredths > 9 ? hundredths : "0" + hundredths);
-        // Call startTimeout again to update the clock to the next second fraction
-        startTimeout();
+    /* This section inserts the count. The long ternary expression is determining whether to add a leading 0 to each part of the 00:00:00 clock */
+    clock.textContent = (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds) + ":" + (hundredths > 9 ? hundredths : "0" + hundredths);
+    /* Now start counting */
+    startTimeout();
 }
 
-// Timer funtion
-function startTimeout() {
-    // Call updateClock every hundredth of a second, ie every 10 milliseconds
-    t = setTimeout(updateClock, 10);
+/* The clock timer */
+function startTimeout(){clockTimer = setTimeout(updateClock, 10);}
+
+/* Stop the clock and the star rating timer */
+function stopClock(){
+    clearTimeout(clockTimer);
+    clearInterval(starHandlerTimer);
 }
 
-// Stop function
-function stopClock() {
-    // Clear the setTimeout
-    clearTimeout(t);
-}
-
-// Clear function
+/* Reset the clock and reset clock counters */
 function clearClock() {
-    // Reset the clock text
     clock.textContent = "00:00:00";
-    // Reset the counters
     hundredths = 0;
     seconds = 0;
     minutes = 0;
 }
 
-/*
- * HELPERS
- */
-
-// I wanted a function that will remove one or more classes from one or more elements - a node or a node list, so I created a couple of helper functions.
-function removeClasses(theElements, ...theClasses) {
-    /* Test to see if "theElements" is a single node or a node list. Credit to Tim Pietrusky https://twitter.com/jackrugile/status/435539724774547456. NodeList has a method called item() that node does not. If the type of something.item is undefined, then you're not dealing with a NodeList. */
-    if (typeof theElements.item === "undefined") {
-        // Iterate over the "theClasses" rest parameter
-        for (const theClass of theClasses) {
-            // Use classList.remove() to remove each class
-            theElements.classList.remove(theClass);
-        }
-    } else {
-        // When dealing with a node list, iterate over each node
-        for (const theElement of theElements) {
-            // Iterate over the "theClasses" rest parameter
-            for (const theClass of theClasses) {
-                // Use classList.remove() to remove each class
-                theElement.classList.remove(theClass);
-            }
-        }
-    }
+/* Start the star rating timer */
+function starRating () {
+    /* We'll decrement the stars by halfs every 20 seconds */
+    starHandlerTimer = setInterval(starHandler, 20000);
 }
 
-//Now I need one to add one or more classes to one or more elements
-function addClasses(theElements, ...theClasses) {
-    /* Test to see if "theElements" is a single node or a node list. Credit to Tim Pietrusky https://twitter.com/jackrugile/status/435539724774547456. NodeList has a method called item() that node does not. If the type of something.item is undefined, then you're not dealing with a NodeList. */
-    if (typeof theElements.item === "undefined") {
-        // Iterate over the "theClasses" rest parameter
-        for (const theClass of theClasses) {
-            // Use classList.add() to add each class
-            theElements.classList.add(theClass);
-        }
+/* The star decrementing function */
+function starHandler() { 
+    if (i % 2 === 0) {
+        /* If the counter is even, then change the star to a half star  by swappin the fa class */
+        let theStar = starsLis[(i/2) - 1].firstElementChild;
+        theStar.classList.remove("fa-star");
+        theStar.classList.add("fa-star-half");
     } else {
-        // When dealing with a node list, iterate over each node
-        for (const theElement of theElements) {
-            // Iterate over the "theClasses" rest parameter
-            for (const theClass of theClasses) {
-                // Use classList.add() to add each class
-                theElement.classList.add(theClass);
-            }
-        }
+        /* If the counter is odd, then change the half star back to a star and hide it */
+        let theStar = starsLis[((i+1)/2) - 1].firstElementChild;
+        theStar.classList.remove("fa-star-half");
+        theStar.classList.add("fa-star", "hide");
     }
+    /* Decrement the counter. The counter is declared up at the top under VARIABLES */
+    i--;
 }
 
-/*
- * FUNCTION CALLS AND LISTENERS
- */
+/* Show the modal with the star rating and time */
+function modalSetup () {
+    const modalRating = document.getElementById("modalRating");
+    const modalTime = document.getElementById("modalTime");
+    modalRating.textContent = "Your star rating is: ";
+    modalTime.textContent = "Your time is: ";
+    const clnStarsUl = starsUl.cloneNode(true);
+    const clnClock = clock.cloneNode(true);
+    modalRating.appendChild(clnStarsUl);
+    modalTime.appendChild(clnClock);
+    playAgainModal.classList.add("show-modal");
+}
 
-// Shuffle the cards and display the board
+/* Call setup */
 setup();
-
-// If a card is clicked, "flip" it.
+/* Add our event listeners for the play button, play again button, and reset */
 deck.addEventListener("click", turn, false);
-
-// If the game is won or the player hits reset, then set up a new game
-document.getElementById("reset").addEventListener("click", setup, false);
-document.getElementById("play-again").addEventListener("click", setup, false);
-
-
-
-//const star = document.createElement("li");
-//star.innerHTML = '<i class="fa fa-star"></i>';
-//document.getElementById('stars').appendChild(star);
-
+reset.addEventListener("click", setup, false);
+playBttn.addEventListener("click", setup, false);
+playAgainBttn.addEventListener("click", setup, false);
